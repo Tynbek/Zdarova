00afbd1ea0bcf9e375e5795f5463debb9ca4f2b5
Tynbek <alexsey.131999@gmail.com>
2022-11-23 01:39:38 +0300

    homework

--- 3/3.py
+++ 3/3.py
@@ -2,27 +2,25 @@
 # приветственное сообщение путем форматирования 3-мя способам
 
 #1 Способ
-# name = input('Enter your name: ')                         #Не очень понимаю как это через конкатенацию провернуть
-# age = input('Enter your age: ')                           # Ток так смог
+# name = input('Enter your name: ')                             #Не очень понимаю как это через конкатенацию провернуть
+# age = input('Enter your age: ')                               # Ток так смог
 # town = input('Enter your town: ')
-# greeting = 'HELLO! '
-# full = greeting + name + ' ' + age + ' ' + town
-# print(input(full))
+# full_info = "Hello " + name + ' ' + 'you ' + age + ' ' + 'you are from ' + town
+# print(input(full_info))
 
 #2 Способ
 
-# name = input('Enter your name: ')
-# age = input('Enter your age: ')
-# town = input('Enter your town: ')
-#
-# text = 'Hello {}, age {}, town {}'.format(name, age, town)
-# print(text, end=' bye:)')
+name = input('Enter your name: ')                                       #Для чего тут нужно именовать? По типу
+age = input('Enter your age: ')                                         #name=name и тд
+town = input('Enter your town: ')
+
+text = 'Hello {name}, you {age}, you are from {town}'.format(name=name, age=age, town=town)
+print(text, end=' bye:)')
 
 #3 Способ
 
 # name = input('Enter your name: ')
 # age = input('Enter your age: ')
 # town = input('Enter your town: ')
-#
-# text = f'Hello {name}, age {age}, town {town}'
+# text = f'Hello {name}, you {age}, you are from {town}'
 # print(text)
