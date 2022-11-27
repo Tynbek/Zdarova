58c0096582007a0960120179c9b5b606606ac6da
Tynbek <alexsey.131999@gmail.com>
2022-11-28 02:02:24 +0300

    5 lesson

---
+++ all lessons/5 lesson.py
@@ -0,0 +1,54 @@
+# word = input('Vvedite: ')
+
+# if word.isdigit():  #Оператор ветвления
+#     word = int(word)
+#     print('digit')
+# elif word.isalpha():
+#     print('word')
+# elif word.isalnum():
+#     print('letters and numbers')
+# else:
+#     print('Lox')
+
+# numbers = [1, 2]
+#
+# if not numbers:
+#     pass
+
+# number = 4
+# is_even = 'нечетное' if number % 2 else 'четное'  #Тернарный оператор
+#
+# if number % 2:    #Проверка на четность
+#     is_even = False
+# else:
+#     is_even = True
+# text = 'hello'
+# res = 'слово' if text.isalpha else 'число' if text.isdigit() else 'не слово' #Тернарный оператор
+# # if text.isalpha():
+# #     res = 'слово'
+# # else:
+# #     res = 'не слово'
+# print(res)
+
+#Булева алгебра
+
+# text = 'hello'
+#
+# if text.isalpha() or text.isalnum() and text.islower():
+#     pass
+# if text.isalpha() and text.isalnum():
+#     pass
+
+
+# x = True                #Оч клёвая задачка по таблице истинности
+# y = False
+# z = False
+# if not x or y:
+#     print(1)
+# elif not x or not y and z:
+#     print(2)
+# elif not x or not y and z:
+#     print(3)
+# else:
+#     print(4)
+
