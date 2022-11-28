9f713dbce13b61aa99b1bb38854fe78bcbb3882c
Tynbek <alexsey.131999@gmail.com>
2022-11-28 18:59:25 +0300

    5 lesson

--- all lessons/5 lesson.py
+++ all lessons/5 lesson.py
@@ -52,3 +52,84 @@
 # else:
 #     print(4)
 
+#Циклы
+
+# for i in range(1, 10, 2):
+#     i **= 2
+#     print(i)
+
+# numbers = [2, 5, 3, 5, 6, 9]
+
+ #итерация по индексам(для строк, списков, кортежей)
+# for i in range(len(numbers)):
+#     print(numbers[1])
+
+#итерация по элементам(для люой коллекции)
+# for number in numbers:
+#     print(number)
+
+#итерация по упорядоченной коллекции(строка, список, кортеж)
+# for i, val in enumerate(numbers): #Возвращает кортеж с 2-мя эл-ми(индекс, значение на этом индексе)
+    # print(i, val)
+    # print(f'на индексе: {i} значение: {val}')
+    # print({i: val})
+
+# По словарям
+
+# data = {
+#     'name': 'vasya',
+#     'email': 'vasya@info.com'
+#}
+
+# for key in data: #Итерация по ключам
+#     print(key)
+# for key in data.values(): #По значению
+#     print(key)
+# for key, val in data.items(): #по ключам и значениям
+#     print(key, val)
+
+# numbers = [4, 6, 0, 3, 7, 4, 0]
+# for number in numbers:
+#     if not number:  #if number == 0
+#         continue
+#     number **= 2
+#     print(number)
+
+# numbers = [4, 6, 0, 3, 7, 4, 0]
+# for i, number in enumerate(numbers):
+#     if number == 0:
+#         continue  #переходит сразу  к слдующей итерации
+#     numbers[i] = number ** 2
+#     print(numbers)
+
+# numbers = [4, 6, 0, 3, 7, 4, 0]
+# for i, number in enumerate(numbers):
+#     if number == 0:
+#         break   #прерывает выполнение цикла
+#     numbers[i] = number ** 2
+# else:
+#     print('закончился самостоятельно')
+# print(numbers)
+
+# n = 100
+# while n:
+#     n -= 1
+#     print(n)
+
+# users = {
+#     1: {
+#         'name': 'name1',
+#         'email': 'email1'
+#     },
+#
+#     2: {
+#                 'name': 'name2',
+#                 'email': 'email2'
+#     }
+# }
+
+# for _ in range(10):
+#     print('hello world!')
+#     print(_)
+
+
